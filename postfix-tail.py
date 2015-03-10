5fa145f8e818c112cedf1f3fcaceb8b16f656406
tivrobo <tivrobo@users.noreply.github.com>
2015-03-10 12:09:22 +0200

    few updates

--- postfix-tail.py
+++ postfix-tail.py
@@ -33,19 +33,22 @@ class LogTail:
                     time.sleep(0.1)
                     self.f.seek(self.pos)
             else:
-                """print, return or otherwise manipulate
-                the tailed line"""
-                p = re.compile("([0-9A-F]{10})\:")
-                qid = p.findall(line)
-                if len(qid) >= 1:
-                    queueid = qid[0]
-                    qdic.setdefault(queueid,[]).append(line.rstrip())
-                p = re.compile("([0-9A-F]{10})\: (?:removed|milter-reject)")
-                qid = p.findall(line)
-                if len(qid) >= 1:
-                    queueid = qid[0]
-                    print "; ".join(qdic[queueid])
-                    del qdic[queueid]
+                p = re.compile("^(.*)\/(.*)\[(.*)\]: ([0-9A-F]{10}): (.*)$")
+                pline = p.search(line)
+                if pline:
+                    syslmsg = pline.group(1)
+                    postcom = pline.group(2)
+                    postpid = pline.group(3)
+                    queueid = pline.group(4)
+                    message = pline.group(5)
+                    qdic.setdefault(queueid,[]).append(postcom.rstrip() + "=" + message.rstrip())
+                    p = re.compile("^.*(removed|milter-reject).*$")
+                    pline = p.match(message)
+                    if pline is not None:
+                        line = [queueid]
+                        print ";".join(line + qdic[queueid])
+                        del qdic[queueid]
 
 objtail = LogTail("/var/log/maillog")
+#objtail = LogTail("C:/Data/Temp/maillog.txt")
 objtail.tail()
